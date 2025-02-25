3ae9dc39765a6d462c94095eb52864724362ccbd
evren <evren@evren.gay>
2025-02-24 21:59:11 -0500

    nixlab configuration.nix

--- hosts/nixlab/configuration.nix
+++ hosts/nixlab/configuration.nix
@@ -1,7 +1,34 @@
 { config, lib, pkgs, ... }: {
 
 	imports = [
-		/etc/nixos/configuration.nix
+		/etc/nixos/hardware-configuration.nix
 	];
+
+	networking.hostName = "nixlab";
+
+	boot.loader.grub.device = "nodev";
+
+	fileSystems."/" = {
+		device = "/dev/disk/by-label/NIXROOT";
+		fsType = "ext4";
+	};
+	fileSystems."/boot" = {
+		device = "/dev/disk/by-label/NIXBOOT";
+		fsType = "vfat";
+	};
+
+	networking.interfaces.enp0s31f6.ipv4.addresses = [{
+		address = "10.1.1.1";
+		prefixLength = 24;
+	}];
+
+	programs = {
+		git.enable = true;
+	};
+
+	services = {
+		openssh.enable = true;
+		tailscale.enable = true;
+	};
 
 }
