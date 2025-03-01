9ea9233766363e1a81420d3e9a57187ab782485b
evren <evren@evren.gay>
2025-03-01 17:10:14 -0500

    systemd-boot

--- hosts/nixlab/configuration.nix
+++ hosts/nixlab/configuration.nix
@@ -7,6 +7,8 @@
 	networking.hostName = "nixlab";
 
 	boot.loader.grub.device = "nodev";
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
 
 	fileSystems."/" = {
 		device = "/dev/disk/by-label/NIXROOT";
