2a75fa6417a036113eacab0e1bb91d11264b3cc5
evren <evren@evren.gay>
2025-04-18 14:39:09 -0400

    misc

--- services/conduit.nix
+++ services/conduit.nix
@@ -4,6 +4,9 @@
 		enable = true;
 		settings.global = {
 			server_name = "evren.gay";
+			port = "6060";
+			allow_registration = true;
+			registration_token = "token";
 			well_known = {
 				client = "https://matrix.evren.gay";
 			};
