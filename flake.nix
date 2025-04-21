cc2c09eea110edc8c6e217e781c6862b224ee3dd
evren <evren@evren.gay>
2025-04-20 21:52:01 -0400

    misc

--- flake.nix
+++ flake.nix
@@ -73,11 +73,11 @@
 				./themes
 
 				# (hmModule "msi")
-				home-manager.nixosModules.home-manager {
-					home-manager.useGlobalPkgs = true;
-					home-manager.useUserPackages = true;
-					home-manager.users.evren = ./hosts/msi/home-manager/home.nix;
-				}
+				# home-manager.nixosModules.home-manager {
+				# 	home-manager.useGlobalPkgs = true;
+				# 	home-manager.useUserPackages = true;
+				# 	home-manager.users.evren = ./hosts/msi/home-manager/home.nix;
+				# }
 			];
 
 			# nixlab = lib.nixosSystem ( common // {
