285c11ca4ce78a69bd1dc0551430013a61b3fabd
evren <evren@evren.gay>
2025-04-20 22:33:55 -0400

    misc

--- hosts/msi/nixos/configuration.nix
+++ hosts/msi/nixos/configuration.nix
@@ -21,14 +21,19 @@
 		nrs = "sudo nixos-rebuild switch --flake /home/evren/flake#msi";
 	};
 
-	hardware.graphics = {
-		enable = true;
-		extraPackages = with pkgs; [ nvidia-vaapi-driver ];
-	};
 	services.xserver.videoDrivers = [ "nvidia" ];
-	hardware.nvidia.prime = {
-		intelBusId = "PCI:0:2:0";
-		nvidiaBusId = "PCI:1:0:0";
+	hardware = {
+		graphics = {
+			enable = true;
+			extraPackages = [ pkgs.nvidia-vaapi-driver ];
+		};
+		nvidia = {
+			open = true;
+			prime = {
+				intelBusId = "PCI:0:2:0";
+				nvidiaBusId = "PCI:1:0:0";
+			};
+		};
 	};
 
 }
